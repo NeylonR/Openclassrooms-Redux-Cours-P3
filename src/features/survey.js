68c12c3d64a46eba3f7473c143d49d6745678de4
NeylonR <rodrigues.neylon.0@gmail.com>
2022-10-07 18:12:16 +0200

    exercice 3(forgot to do the survey slice) part 4

--- src/features/survey.js
+++ src/features/survey.js
@@ -1,4 +1,4 @@
-import { createAction, createReducer } from "@reduxjs/toolkit";
+import { createSlice} from "@reduxjs/toolkit";
 import { selectSurvey } from "../utils/selectors";
 
 const initialState = {
@@ -7,63 +7,67 @@ const initialState = {
     error: null,
 };
 
-const surveyFetching = createAction('survey/fetching');
-const surveyResolved = createAction(
-    'survey/resolved',
-    (data) => ({ payload: {data}})
-);
-const surveyRejected = createAction(
-    'survey/rejected',
-    (error) => ({ payload: {error}})
-);
+const { actions, reducer } = createSlice({
+    name: 'survey',
+    initialState: initialState,
+    reducers: {
+        fetching: (draft, action) => {
+            if(draft.status === 'void'){
+                draft.status = 'pending';
+                return
+            };
+            if(draft.status === 'rejected'){
+                draft.error = null;
+                draft.status = 'pending';
+                return
+            };
+            if(draft.status === 'resolved'){
+                draft.status = 'updating';
+                return
+            };
+            return;
+        },
+        resolved: {
+            prepare: (data) => ({ payload: {data}}),
+            reducer: (draft, action) => {
+                if(draft.status === 'pending' || draft.status === 'updating'){
+                    draft.data = action.payload.data;
+                    draft.status = 'resolved';
+                    return;
+                }
+                return;
+            },
+        },
+        rejected: {
+            prepare: (error) => ({ payload: {error}}),
+            reducer: (draft, action) => {
+                if(draft.status === 'pending' || draft.status === 'updating'){
+                    draft.error = action.payload.error;
+                    draft.data = null;
+                    draft.status = 'rejected';
+                    return;
+                }
+                return;
+            },
+        },
+    }
+})
 
 export async function fetchOrUpdateSurvey(dispatch, getState){
     const status = selectSurvey(getState()).status;
     if (status === 'pending' || status === 'updating') {
       return;
     }
-    dispatch(surveyFetching());
+    dispatch(fetching());
     try {
       const response = await fetch('http://localhost:8000/survey');
       const data = await response.json();
-      dispatch(surveyResolved(data));
+      dispatch(resolved(data));
     } catch (error) {
-      dispatch(surveyRejected(error));
+      dispatch(rejected(error));
     }
 };
 
-export default createReducer(initialState, builder => builder 
-    .addCase(surveyFetching, (draft, action) => {
-        if(draft.status === 'void'){
-            draft.status = 'pending';
-            return
-        };
-        if(draft.status === 'rejected'){
-            draft.error = null;
-            draft.status = 'pending';
-            return
-        };
-        if(draft.status === 'resolved'){
-            draft.status = 'updating';
-            return
-        };
-        return;
-    })
-    .addCase(surveyResolved, (draft, action) => {
-        if(draft.status === 'pending' || draft.status === 'updating'){
-            draft.data = action.payload.data;
-            draft.status = 'resolved';
-            return;
-        }
-        return;
-    })
-    .addCase(surveyRejected, (draft, action) => {
-        if(draft.status === 'pending' || draft.status === 'updating'){
-            draft.error = action.payload.error;
-            draft.data = null;
-            draft.status = 'rejected';
-            return;
-        }
-        return;
-    })
-);
+const { fetching, resolved, rejected } = actions;
+
+export default reducer;
